dd6a02e7d5d635311c95ba6e7d8621b88f4ded8c
Anders Pedersen <anp@conscia.com>
2025-09-08 13:54:17 +0200

    moved OVHcloud cluster to Paris

--- 1-managed-k8s/ovhcloud/main.tf
+++ 1-managed-k8s/ovhcloud/main.tf
@@ -1,24 +1,60 @@
-resource "ovh_cloud_project_kube" "cluster" {
+resource "ovh_cloud_project_network_private" "net" {
   service_name = var.service_name
-  name         = var.cluster.name
+  name         = var.network.name
+  regions      = [var.ovh_region]
+}
+
+locals {
+  network_uuid = tolist(
+    ovh_cloud_project_network_private.net.regions_attributes[*].openstackid
+  )[0]
+}
+
+resource "ovh_cloud_project_network_private_subnet" "subnet" {
+  service_name = var.service_name
+  network_id   = ovh_cloud_project_network_private.net.id
   region       = var.ovh_region
-  version      = var.cluster.version
+  start        = var.network.start
+  end          = var.network.end
+  network      = var.network.network
+  dhcp         = true
+  no_gateway   = false
+}
+
+resource "ovh_cloud_project_gateway" "gateway" {
+  service_name = var.service_name
+  name         = "gateway"
+  model        = var.network.gateway_model
+  region       = var.ovh_region
+  network_id   = local.network_uuid
+  subnet_id    = ovh_cloud_project_network_private_subnet.subnet.id
+}
+
+resource "ovh_cloud_project_kube" "cluster" {
+  service_name       = var.service_name
+  name               = var.cluster.name
+  region             = var.ovh_region
+  version            = var.cluster.version
+  private_network_id = local.network_uuid
+  nodes_subnet_id    = ovh_cloud_project_network_private_subnet.subnet.id
+  depends_on         = [ovh_cloud_project_gateway.gateway] //Gateway is mandatory for multizones cluster
 }
 
 resource "ovh_cloud_project_kube_nodepool" "node_pool" {
-  service_name  = var.service_name
-  kube_id       = ovh_cloud_project_kube.cluster.id
-  name          = "${var.cluster.name}-pool" //Warning: "_" char is not allowed!
-  flavor_name   = var.cluster.node_type
-  desired_nodes = 1
-  max_nodes     = 1
-  min_nodes     = 1
+  service_name       = var.service_name
+  kube_id            = ovh_cloud_project_kube.cluster.id
+  name               = "${var.cluster.name}-nodepool"
+  flavor_name        = var.cluster.node_type
+  desired_nodes      = 1
+  max_nodes          = 1
+  min_nodes          = 1
+  availability_zones = [var.ovh_az] //Currently, only one zone is supported per pool
 }
 
 data "ovh_cloud_project_capabilities_containerregistry_filter" "capabilities" {
   service_name = var.service_name
-  plan_name    = var.registry.plan
-  region       = var.registry.region
+  plan_name    = var.registry_plan
+  region       = var.ovh_region
 }
 
 resource "ovh_cloud_project_containerregistry" "registry" {
--- 1-managed-k8s/ovhcloud/variables.tf
+++ 1-managed-k8s/ovhcloud/variables.tf
@@ -10,8 +10,11 @@ variable "service_name" {
 }
 
 variable "ovh_region" {
-  # default = "GRA9" # Gravelines, France
-  default = "UK1" # London, France
+  default = "EU-WEST-PAR" # Gravelines, France "GRA9", London "UK1", France "EU-WEST-PAR"
+}
+
+variable "ovh_az" {
+  default = "eu-west-par-a"
 }
 
 variable "cluster" {
@@ -22,21 +25,31 @@ variable "cluster" {
   })
   default = {
     name      = "sandbox-cluster"
-    version   = "1.32"
-    node_type = "b2-7"
+    version   = "1.33"
+    node_type = "b3-8"
+  }
+}
+
+variable "network" {
+  type = object({
+    name          = string
+    network       = string
+    start         = string
+    end           = string
+    gateway_model = string
+  })
+  default = {
+    name          = "sandbox-network"
+    network       = "10.2.0.0/16"
+    start         = "10.2.0.2"
+    end           = "10.2.255.254"
+    gateway_model = "s"
   }
 }
 
 
-variable "registry" {
-  type = object({
-    region = string
-    plan   = string
-  })
-  default = {
-    region = "GRA"
-    plan   = "SMALL"
-  }
+variable "registry_plan" {
+  default = "SMALL"
 }
 
 variable "registry_email" {}
